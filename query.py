dc1e0d635a6e473556197620a6b57fbd213d1b83
Evan <fuck@off.com>
2024-03-18 20:47:55 -0400

    Added listing movies to user's capabilities
    User is now told to enter "list" to show all movies.
    All movies are listed and numbered in a readable format.
    User options are redisplayed afterwards.
     #2

--- query.py
+++ query.py
@@ -6,6 +6,7 @@ def displayMenu():
     print()
     print("Menu")
     print("* enter q for exit")
+    print("* enter list for listing all the movie names")
     print()
 
 def main():
@@ -14,6 +15,12 @@ def main():
         inVar = input()
         if(inVar == "q"):
             break
+        elif(inVar.lower() == "list"):
+            i = 1
+            for movie in movies._movies:
+                print(str(i) + " " + movie["name"])
+                i += 1
+
 
 if __name__ == "__main__":
     main()

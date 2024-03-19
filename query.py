01b446a0ba0b9d067073d71e836c43892b416168
Evan <fuck@off.com>
2024-03-18 21:25:55 -0400

    Allowed for a user to find a movie by cast names
    An option was added to the user menu to inform them of how they can search for a movie by cast.
    added the search functionality for a user to find a movie by cast.

--- query.py
+++ query.py
@@ -8,6 +8,7 @@ def displayMenu():
     print("* enter q for exit")
     print("* enter list for listing all the movie names")
     print("* enter search for searching movies")
+    print("* enter cast for searching cast")
     print()
 
 def main():
@@ -26,6 +27,31 @@ def main():
             for movie in movies._movies:
                 if (searchingFor.lower() in movie["name"].lower() ):
                     print(movie["name"])
+        elif(inVar.lower() == "cast"):
+            searchingFor = input("Please enter the search term: ")
+            hasResults = False
+            for movie in movies._movies:
+                found = False
+                first = True
+                for actor in movie["cast"]:
+                    if (searchingFor.lower() in actor.lower()):
+                        found = True
+                        hasResults = True
+                        break
+                if (not found):
+                    continue
+                print(movie["name"])
+                for actor in movie["cast"]:
+                    if (searchingFor.lower() in actor.lower()):
+                        if (first):
+                            print("['", end="")
+                            first = False
+                        else:
+                            print(", '", end ="")
+                        print(actor, end="'")
+                print("]")
+            if (not hasResults):
+                print("No Result.")
 
 
 
